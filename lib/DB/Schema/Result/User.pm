169cf498a9757bf7b8574be1b03cf3612545d887
s4mur4i <s4mur4i@hanoi>
2014-01-13 21:50:58 +0100

    immutable fix

--- lib/DB/Schema/Result/User.pm
+++ lib/DB/Schema/Result/User.pm
@@ -155,5 +155,5 @@ sub set_all_roles {
 }
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 1;
