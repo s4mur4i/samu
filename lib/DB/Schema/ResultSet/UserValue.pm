f130e185b906158c5b39bec5015366bd283ade9e
Fayland Lam <fayland@gmail.com>
2014-02-24 21:45:47 +0800

    add Crypt::CBC

--- lib/DB/Schema/ResultSet/UserValue.pm
+++ lib/DB/Schema/ResultSet/UserValue.pm
@@ -3,6 +3,15 @@ package DB::Schema::ResultSet::UserValue;
 use strict;
 use warnings;
 use base 'DBIx::Class::ResultSet';
+use Crypt::CBC;
+
+our $SECRET_KEY = 'somethinghardtoguess';
+sub __get_crypt {
+    return Crypt::CBC->new(
+        -key    => 'my secret key',
+        -cipher => 'Blowfish'
+    );
+}
 
 sub get_user_values {
     my ($self, $user_id) = @_;
@@ -10,10 +19,30 @@ sub get_user_values {
     my %data;
     my $rs = $self->search({ user_id => $user_id });
     while (my $r = $rs->next) {
-        $data{ $r->value->name } = $r->data;
+        my $name = $r->value->name;
+        next if $name =~ /password/; # skip password?
+        $data{ $name } = $r->data;
     }
 
     return wantarray ? %data : \%data;
+}
+
+sub get_user_value {
+    my ($self, $user_id, $name) = @_;
+
+    my $schema = $self->result_source->schema;
+
+    my $vr = $schema->resultset('Value')->find({ name => $name });
+    return unless $vr;
+
+    my $r = $self->find({ user_id => $user_id, value_id => $vr->id });
+    return unless $r;
+
+    if ($name =~ /password/) {
+        return __get_crypt()->decrypt($r->data);
+    } else {
+        return $r->data;
+    }
 }
 
 sub set_user_value {
@@ -23,6 +52,10 @@ sub set_user_value {
 
     my $vr = $schema->resultset('Value')->find({ name => $name });
     return unless $vr; # or create it on the fly? TODO
+
+    if ($name =~ /password/) { # or use eq
+        $value = __get_crypt()->encrypt($value);
+    }
 
     my $r = $self->find({ user_id => $user_id, value_id => $vr->id });
     if ($r) {
