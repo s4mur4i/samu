855426bf37e3c8c02918995708bc0ae30c6a2004
S4mur4i <admin@szamuraj.com>
2014-03-11 21:01:21 +0100

    refactored exception

--- lib/SamuAPI/Exception.pm
+++ lib/SamuAPI/Exception.pm
@@ -59,24 +59,7 @@ sub parse_ex {
     my $info = {};
     if ( $ex->isa('SoapFault')) {
         # We have a vmware exception
-        if ( $ex->{name} eq "DuplicateNameFault") {
-            $info = &vmwareDuplicateName($ex);
-        } elsif ( $ex->{name} eq "InsufficientResourcesFault" ) {
-            $info = &vmwareInsufficientResourcesFault($ex);
-        } elsif ( $ex->{name} eq "InvalidArgumentFault" ) {
-            $info = &vmwareInvalidArgument($ex);
-        } elsif ( $ex->{name} eq 'InvalidRequestFault' ) {
-            $info = &vmwareInvalidRequest($ex);
-        } elsif ( $ex->{name} eq "InvalidNameFault" ) {
-            $info = &vmwareInvalidName($ex);
-        } elsif ($ex->{name} eq "NotSupportedFault") {
-            $info = &vmwareNotSupported($ex);
-        } elsif ( $ex->{name} eq "RuntimeFaultFault") {
-            $info = &vmwareRuntimeFault($ex);
-        } else {
-            $info = { unknown => "unknown_vmware"};
-            print Dumper $ex;
-        }
+        $info = &object2hash($ex);
     } elsif ( $ex->isa('ExBase') ) {
         # We have  SamuAPI exception
         for my $key ( keys %$ex ) {
@@ -92,64 +75,16 @@ sub parse_ex {
     return $info;
 }
 
-sub vmwareDuplicateName {
-    my $ex = shift;
-    my $return = {};
-    $return->{name} = $ex->{name};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{detail} = { name => $ex->{detail}->{name} };
-    $return->{detail}->{object} = { value => $ex->{detail}->{object}->{value}, type => $ex->{detail}->{object}->{type}};
-    return $return;
-}
-
-sub vmwareInsufficientResourcesFault {
-    my $ex = shift;
-    my $return = {};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{name} = $ex->{name};
-    return $return;
-}
-
-sub vmwareInvalidArgument {
-    my $ex = shift;
-    my $return = {};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{name} = $ex->{name};
-    $return->{invalidProperty} = $ex->{invalidProperty};
-    return $return;
-}
-
-sub vmwareInvalidRequest {
-    my $ex = shift;
-    my $return = {};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{name} = $ex->{name};
-    return $return;
-}
-
-
-sub vmwareInvalidName {
-    my $ex = shift;
-    my $return = {};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{name} = $ex->{name};
-    $return->{entity} = $ex->{entity};
-    return $return;
-}
-
-sub vmwareNotSupported {
-    my $ex = shift;
-    my $return = {};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{name} = $ex->{name};
-    return $return;
-}
-
-sub vmwareRuntimeFault {
-    my $ex = shift;
-    my $return = {};
-    $return->{fault_string} = $ex->{fault_string};
-    $return->{name} = $ex->{name};
-    return $return;
+sub _object2hash {
+    my $obj = shift;
+    my $hash = {};
+    for my $key ( keys $obj ) {
+        if ( ref($obj->{$key} eq "HASH")) {
+            $hash->{$key} = &_object2hash($obj->{$key});
+        } else {
+            $hash->{$key} = $obj->{$key};
+        }
+    }
+    return $hash;
 }
 1
