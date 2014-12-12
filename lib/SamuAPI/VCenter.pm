78879f62407824108f8c9b3280e197a148fc62c1
Krisztian Banhidy <s4mur4i@balabit.hu>
2014-12-12 10:50:00 +0100

    Vcenter: corrected vm_delete argument passing

--- lib/SamuAPI/VCenter.pm
+++ lib/SamuAPI/VCenter.pm
@@ -2885,7 +2885,7 @@ sub destroy {
     my ($self,%args) = @_;
     $self->{logger}->start;
     my $return= {};
-    my $view = $self->values_to_view( %args );
+    my $view = $self->values_to_view( type=> 'VirtualMachine',  value => $args{moref_value} );
     my $vm = SamuAPI_virtualmachine->new( view => $view, logger => $self->{logger} );
     eval {
         $return = [$self->destroy_entity( obj => $vm )];
