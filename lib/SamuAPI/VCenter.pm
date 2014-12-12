8fdd2b1565abc9dc2d42be734e27fdd0e6ba665f
Krisztian Banhidy <s4mur4i@balabit.hu>
2014-12-12 11:42:15 +0100

    destroy_vm: values_to_view used wrong argument for invoking

--- lib/SamuAPI/VCenter.pm
+++ lib/SamuAPI/VCenter.pm
@@ -2886,7 +2886,7 @@ sub destroy {
     my ($self,%args) = @_;
     $self->{logger}->start;
     my $return= {};
-    my $view = $self->values_to_view( type => 'VirtualMachine', value => $args{moref});
+    my $view = $self->values_to_view( type => 'VirtualMachine', value => $args{moref_value});
     my $vm = SamuAPI_virtualmachine->new( view => $view, logger => $self->{logger} );
     eval {
         $return = [$self->destroy_entity( obj => $vm )];
